0dcbe04d9cadabe2032e399aa323cf7d25d1411a
pmallige-dev <72284504+pmallige-dev@users.noreply.github.com>
2022-05-23 09:11:05 +0530

    Addiing the timer

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const word = document.getElementById('word');
 const text = document.getElementById('text');
 const scoreEl = document.getElementById('score');
 const timeEl = document.getElementById('time');
-const endgameEl = document.getElementById('end-game');
+const endgameEl = document.getElementById('end-game-container');
 const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
@@ -44,6 +44,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000)
+
 // Generate random word from array
 function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)]
@@ -61,6 +67,30 @@ function updateScore() {
     scoreEl.innerHTML = score;
 }
 
+// Update Time
+function updateTime() {
+    time--;
+    timeEl.innerHTML = time + 's';
+
+    if (time === 0) {
+        clearInterval(timeInterval);
+
+        //end game
+        gameOver();
+    }
+}
+
+// Game over, show end screen'
+function gameOver() {
+    endgameEl.innerHTML = `
+        <h1>Time Out</h1>
+        <p>Your Final Score is ${score}</p>
+        <button onClick="location.reload()">Reload!</button>
+    `;
+
+    endgameEl.style.display = 'flex'
+}
+
 addWordToDOM();
 
 // Event Listeners
@@ -73,5 +103,9 @@ text.addEventListener('input', e => {
 
         // Clear
         e.target.value = '';
+
+        time += 5;
+
+        updateTime();
     }
 })
